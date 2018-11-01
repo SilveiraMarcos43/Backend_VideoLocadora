2cf13ff6a79b52b99ced7271fc3d52fb9d0c7de8
JV01\aleite <aleite@JVN6010133>
2018-11-01 13:35:50 -0300

    Adicionado mediasLocation

---
+++ src/2 - Domain/SVL.Domain.Location/Domain/MediasLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVL.Domain.Location.Domain
+{
+    public class MediasLocation
+    {
+        private int _locationId;
+        private int _mediaId;
+    }
+}
